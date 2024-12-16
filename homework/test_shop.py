22ba2b458dc2230d9527cd70a1f639710b145b16
Sema Butsyk <semakontakt@mail.ru>
2024-12-16 11:21:08 +0500

    homework v3

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -7,17 +7,21 @@ from homework.models import Product
 from homework.models import Cart
 
 
+
 @pytest.fixture
-def product():
+def book():
     return Product("book", 100, "This is a book", 1000)
 
 @pytest.fixture
-def product_2():
+def pencile():
     return Product("pencile", 15.50, "This is a pencile", 60)
 
 @pytest.fixture
 def cart():
     return Cart()
+
+buy_product_count = 15
+buy_product_count_2 = 7
 
 class TestProducts:
     """
@@ -29,12 +33,13 @@ class TestProducts:
         # TODO напишите проверки на метод check_quantity
 
         assert book.check_quantity(book.quantity)
-        assert book.check_quantity( - 1)
-        assert not book.check_quantity( + 1)
+        assert book.check_quantity(book.quantity - 1)
+        assert not book.check_quantity(book.quantity + 1)
 
-    def test_product_buy(self, book, required_quantity=None):
+    def test_product_buy(self, book):
         # TODO напишите проверки на метод buy
         quantity_before = book.quantity
+        required_quantity = 1000
         book.buy(required_quantity)
         assert book.quantity == quantity_before - required_quantity
 
@@ -55,15 +60,15 @@ class TestCart:
 
     def test_add_product_in_cart(self, cart, book):
 
-        cart.add_product(book, 15)
-        cart.add_product(book, 35)
+        cart.add_product(book, buy_product_count)
+        cart.add_product(book, buy_product_count + 35)
         assert cart.products[book] == 50
         assert book in cart.products.keys()
 
     def test_remove_product(self, cart, book, pencile):
 
-        cart.add_product(book, 41)
-        cart.add_product(pencile, 7)
+        cart.add_product(book, buy_product_count)
+        cart.add_product(pencile, buy_product_count_2)
         assert book, pencile in cart.products.keys()
         cart.remove_product(book)
         cart.remove_product(pencile)
@@ -71,36 +76,36 @@ class TestCart:
 
     def test_remove_product_partially(self, cart, book, pencile):
 
-        cart.add_product(book, 10)
-        cart.add_product(pencile, 7)
+        cart.add_product(book, buy_product_count)
+        cart.add_product(pencile, buy_product_count_2)
         assert book, pencile in cart.products.keys()
         cart.remove_product(book, 5)
         cart.remove_product(pencile, 3)
-        assert cart.products[book] == 5
+        assert cart.products[book] == 10
         assert cart.products[pencile] == 4
 
     def test_clear_cart(self, cart, book, pencile):
 
-        cart.add_product(book, 2)
-        cart.add_product(pencile, 3)
+        cart.add_product(book, buy_product_count)
+        cart.add_product(pencile, buy_product_count_2)
         cart.clear()
         assert not cart.products
 
     def test_get_total_price(self, cart, book, pencile):
 
-        cart.add_product(book, 154)
-        cart.add_product(pencile, 59)
+        cart.add_product(book, buy_product_count)
+        cart.add_product(pencile, buy_product_count_2)
         assert cart.get_total_price() > 0
 
-    def test_buy(self, cart, book, notebook):
+    def test_buy(self, cart, book, pencile):
 
-        cart.add_product(book, 999)
-        cart.add_product(notebook, 1)
+        cart.add_product(book, buy_product_count)
+        cart.add_product(pencile, buy_product_count_2)
         cart.buy()
         assert not cart.products
 
-    def test_buy_error(self, cart, book, notebook):
+    def test_buy_error(self, cart, book, pencile):
         with pytest.raises(ValueError):
             cart.add_product(book, 1001)
-            cart.add_product(notebook, 61)
+            cart.add_product(pencile, 61)
             cart.buy()
